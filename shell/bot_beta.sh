194b7c14b6f4050870fc2b3f22b96e06756584f6
chiupam <chiupam@126.com>
2022-01-04 14:45:01 +0800

    更新

--- shell/bot_beta.sh
+++ shell/bot_beta.sh
@@ -121,7 +121,6 @@ start() {
   fi
   if [[ -z $(grep -E "123456789" $root/config/bot.json) ]]; then
     if [ -d "/ql" ]; then
-      wget https://gitee.com/chiupam/JD_Diy/raw/main/shell/ql.sh -O /ql/shell/bot.sh
       ps -ef | grep "python3 -m jbot" | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null
       nohup python3 -m jbot > $root/log/bot/bot.log 2>&1 &
     else
